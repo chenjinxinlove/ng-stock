d620e2ce5ce78d8a5feb7f016802ac083a16ccfd
陈金鑫 <505150704@qq.com>
2018-11-22 17:05:06 +0800

    input ----> props

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.less']
 })
 export class ProductComponent implements OnInit {
-
+  private products: Array<Product>;
   constructor() { }
 
   ngOnInit() {
+    this.products = [
+      new Product(1, 'ss1', 1.99, 3.5, 'ssssss', ['ddd', 'sss']),
+      new Product(2, 'ss2', 1.99, 2.5, 'ssssss', ['ddd', 'sss']),
+      new Product(3, 'ss3', 1.99, 4.5, 'ssssss', ['ddd', 'sss']),
+      new Product(4, 'ss4', 1.99, 1.5, 'ssssss', ['ddd', 'sss']),
+      new Product(5, 'ss5', 1.99, 2.5, 'ssssss', ['ddd', 'sss']),
+      new Product(6, 'ss6', 1.99, 3.5, 'ssssss', ['ddd', 'sss']),
+    ];
   }
 
+}
+
+export class Product {
+  constructor(
+    public id: number,
+    public title: string,
+    public price: number,
+    public rating: number,
+    public desc: string,
+    public categoties: string[]
+  ) { }
 }

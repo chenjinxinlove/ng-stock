92a28af143e2496f47e25ec6ef1d9d62ac96f024
陈金鑫 <505150704@qq.com>
2018-11-27 19:55:38 +0800

    完成事件响应

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product, ProductService } from '../shared/product.service';
+import { FormControl } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product',
@@ -8,7 +10,15 @@ import { Product, ProductService } from '../shared/product.service';
 })
 export class ProductComponent implements OnInit {
   private products: Product[];
-  constructor(private productService:  ProductService) { }
+  private keyword: string;
+  private titleFilter: FormControl = new FormControl();
+  constructor(private productService:  ProductService) {
+    this.titleFilter.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe(
+        value => this.keyword = value
+      );
+  }
 
   ngOnInit() {
     this.products = this.productService.getProducts();
